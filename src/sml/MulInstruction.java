29d330774d819611adda511ecae7d506695447ae
Alice <alicegutteridge@gmail.com>
2015-01-18 12:21:49 +0000

    Created sub and mul instructions, changed Translator accordingly

--- src/sml/MulInstruction.java
+++ src/sml/MulInstruction.java
@@ -1,7 +1,7 @@
 package sml;
 
 /**
- * This class is for subtraction instructions
+ * This class is for multiplication instructions
  * 
  * 3 registers are provided as cons arguments and specify
  * the locations of the 2 operands and the result
@@ -9,17 +9,17 @@ package sml;
  * @author agutteridge
  */
 
-public class SubInstruction extends Instruction {
+public class MulInstruction extends Instruction {
 
 	private int result;
 	private int op1;
 	private int op2;
 
-	public SubInstruction(String label, String op) {
+	public MulInstruction(String label, String op) {
 		super(label, op);
 	}
 
-	public SubInstruction(String label, int result, int op1, int op2) {
+	public MulInstruction(String label, int result, int op1, int op2) {
 		this(label, "sub");
 		this.result = result;
 		this.op1 = op1;
@@ -30,11 +30,11 @@ public class SubInstruction extends Instruction {
 	public void execute(Machine m) {
 		int value1 = m.getRegisters().getRegister(op1);
 		int value2 = m.getRegisters().getRegister(op2);
-		m.getRegisters().setRegister(result, value1 - value2);
+		m.getRegisters().setRegister(result, value1 * value2);
 	}
 
 	@Override
 	public String toString() {
-		return super.toString() + " " + op1 + " - " + op2 + " to " + result;
+		return super.toString() + " " + op1 + " * " + op2 + " to " + result;
 	}
 }
